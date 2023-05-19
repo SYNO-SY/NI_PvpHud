54f1f2ae8f0b12365791996871f3e6e8fbb91a1e
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2023-05-19 19:24:23 +0400

    Deleted unwanted shit!!!

--- server.lua
+++
@@ -1,5 +0,0 @@
-Citizen.CreateThread(function()
-	if (GetCurrentResourceName() ~= "NI_PvpHud") then 
-		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named NI_PvpHud for it to work properly!");
-	end
-end)
